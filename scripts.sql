d70848aff35cf7759d21fb8526b4ad38cb6674e0
Эпенди <khadzhiev909@gmail.com>
2023-08-07 16:26:24 +0300

    2 дз

--- scripts.sql
+++ scripts.sql
@@ -2,7 +2,7 @@ select * from student where age between 10 and 20;
 
 select name from student;
 
-select * from student LIKE '%o%';
+select * from student where name LIKE '%o%';
 
 select * from student where age < student.id;
 
